0f01415981ef0e4d3650e6260a0ccbfcb459f4a7
xunita <yayasopeguesoro@gmail.com>
2025-01-13 13:01:12 +0100

    fix 429 rate limit bug

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import MainMyAIAS from './components/body/MainMyAIAS.vue'
+import MainMyAIAS from './components/body/MainMyAIAS.vue';
 const modelSettings = {
   endpoint: 'https://<sasURL>.openai.azure.com/',
   apiKey: 'hhijjkdfdsfkddklopaaakdjkfjdskfjdfslpdfdsdfdsfdsaaFTYuhgJK', // this is not a real api key
@@ -7,7 +7,8 @@ const modelSettings = {
   deployment: 'gpt-4o-mini',
   maxTokens: 800, // if not set, default is 800
 }
-const sysPrompt = '{ "role": "system", "content": [ { "text": "You are a helpfull assistant." } ] }'
+const sysPrompt =
+  '{ "role": "system", "content": [ {"type": "text", "text": "You are a helpfull assistant." } ] }' // add the attribute "type": "text" to the content object to avaid 429 rate limit error
 </script>
 <template>
   <MainMyAIAS :modelSettings="modelSettings" :sysPrompt="sysPrompt" />
